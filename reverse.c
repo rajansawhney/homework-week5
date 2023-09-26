00a5b0b6120e58a57a752f2f9a01289a5a4ef397
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-26 11:00:57 -1000

    reverse comments

--- reverse.c
+++ reverse.c
@@ -3,11 +3,11 @@ Write a C program that reverses a sentence, provided by the user, using recursio
 
 NOTE:
 1. MUST use a recursive function
-3. Display result in the following format ONLY:
-   Reverse of sentence is: <reverse>
+3. Display just the reverse of the sentence on a single line
 
 Example:
 Enter a sentence: awesome possum
-Reverse of sentence is: mussop emosewa
+mussop emosewa
 */
+
 
