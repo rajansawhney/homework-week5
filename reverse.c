ec5b82cb137d4462547e1cb1279d0b9260552b62
rajansawhney <rajan.k.sawhney@gmail.com>
2023-09-26 11:32:04 -1000

    comment updates

--- reverse.c
+++ reverse.c
@@ -3,6 +3,9 @@ Write a C program that reverses a sentence, provided by the user, using recursio
 
 NOTE:
 1. MUST use a recursive function
+2. If reading individual characters:
+   - use getchar() instead of scanf() - or you might run into segmentation faults with auto-grading
+   - to end recursion check if the character is EOF (end of file character) - or you might run into segmentation faults  with auto-grading
 3. Display just the reverse of the sentence on a single line
 
 Example:
